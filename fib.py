f08954a8cdfe468d0a6390398989a4d02f1693c0
Shiva Heidari <heidarishivaa@gmail.com>
2015-10-13 17:19:19 +0330

    Update fib.py

--- fib.py
+++ fib.py
@@ -12,7 +12,11 @@ def fib_recursion(n):
    
     It uses for loop to calculate it.
     '''
-
+def F(x):
+    if x == 0: return 0
+    elif x == 1: return 1
+    else: 
+        return F(x-1)+F(x-2)
 def fib_generator(n):
     '''
     Generator version of fibonacci.
