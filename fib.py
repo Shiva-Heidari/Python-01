4b5b451e6970f2f2fab8ecddef1fb289c41d93e5
Shiva Heidari <heidarishivaa@gmail.com>
2015-10-13 17:34:48 +0330

    Update fib.py
    I solved the second problem( generating fibonacci by recursion) up to now.

--- fib.py
+++ fib.py
@@ -12,6 +12,14 @@ def fib_recursion(n):
    
     It uses for loop to calculate it.
     '''
+    ---------------------------------------
+    def fib_recursion(n):
+    if n == 0: return 0
+    elif n == 1: return 1
+    else: 
+        return F(n-1)+F(n-2)
+    ---------------------------------------
+    
 
 def fib_generator(n):
     '''
